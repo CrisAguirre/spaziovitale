14b88b34def0c1ac1b9721af8a2216a49299d040
Cris <krontroth@gmail.com>
2026-09-07 06:15:10 -0500

    66666

---
+++ public/media/images.js
@@ -0,0 +1,31 @@
+const mediaImages = [
+  { id: 1, url: '/src/assets/media/1.jpg', category: 'cocinas' },
+  { id: 2, url: '/src/assets/media/2024-06-08_Spazio Vitale_1021147046255955.jpg', category: 'locales' },
+  { id: 3, url: '/src/assets/media/2024-07-04_Spazio Vitale_1038395574531102.jpg', category: 'banos' },
+  { id: 4, url: '/src/assets/media/2024-07-24_Spazio Vitale_1051466979890628.jpg', category: 'estudios' },
+  { id: 5, url: '/src/assets/media/2024-07-24_Spazio Vitale_1051466983223961.jpg', category: 'closets' },
+  { id: 6, url: '/src/assets/media/2024-09-25_Spazio Vitale_1096671398703519.jpg', category: 'cocinas' },
+  { id: 7, url: '/src/assets/media/2025-04-08_Spazio Vitale_1251353976568593.jpg', category: 'pergolas' },
+  { id: 8, url: '/src/assets/media/2025-04-15_Spazio Vitale_1257530139284310.jpg', category: 'cocinas' },
+  { id: 9, url: '/src/assets/media/2025-06-28_Spazio Vitale_1317095453327778.jpg', category: 'locales' },
+  { id: 10, url: '/src/assets/media/2025-11-07_Spazio Vitale_1434175018286487.jpg', category: 'banos' },
+  { id: 11, url: '/src/assets/media/2025-11-07_Spazio Vitale_1434175041619818.jpg', category: 'estudios' },
+  { id: 12, url: '/src/assets/media/2025-11-18_Spazio Vitale_1443868883983767.jpg', category: 'closets' },
+  { id: 13, url: '/src/assets/media/2025-11-18_Spazio Vitale_1443868890650433.jpg', category: 'cocinas' },
+  { id: 14, url: '/src/assets/media/2025-11-20_Spazio Vitale_1445594223811233.jpg', category: 'pergolas' },
+  { id: 15, url: '/src/assets/media/2025-11-25_Spazio Vitale_1449812130056109.jpg', category: 'locales' },
+  { id: 16, url: '/src/assets/media/2025-11-27_Spazio Vitale_1451394319897890.jpg', category: 'banos' },
+  { id: 17, url: '/src/assets/media/2025-12-02_Spazio Vitale_1455224239514898.jpg', category: 'estudios' },
+  { id: 18, url: '/src/assets/media/2025-12-02_Spazio Vitale_1455224246181564.jpg', category: 'closets' },
+  { id: 19, url: '/src/assets/media/2025-12-16_Spazio Vitale_1466350501735605.jpg', category: 'cocinas' },
+  { id: 20, url: '/src/assets/media/2025-12-20_Spazio Vitale_1469592164744772.jpg', category: 'locales' },
+  { id: 21, url: '/src/assets/media/2025-12-20_Spazio Vitale_1469592168078105.jpg', category: 'banos' },
+  { id: 22, url: '/src/assets/media/2025-12-20_Spazio Vitale_1469592171411438.jpg', category: 'pergolas' },
+  { id: 23, url: '/src/assets/media/489771814_1250803239957000_8251280459950811323_n.jpg', category: 'cocinas' },
+  { id: 24, url: '/src/assets/media/512086963_1314051426965514_8056330585088352223_n.jpg', category: 'estudios' },
+  { id: 25, url: '/src/assets/media/525166072_1342666380770685_3715776072544267784_n.jpg', category: 'closets' },
+];
+
+export const videoUrl = '/src/assets/media/1.mp4';
+
+export default mediaImages;
